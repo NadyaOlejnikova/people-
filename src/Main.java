ed9c5275dfcc86add00abb7eb021f9ad3074211e
nadyaOleynikova <kposhkanadya@yandex.ru>
2022-09-06 02:34:24 +0700

    commit eraser

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class Main {
     public static void main(String[] args) {
      /*   Ваша задача написать класс-компаратор, который сравнивал бы людей по принципу знатности:
@@ -11,12 +12,15 @@ public class Main {
       */
         List<Person> p1 = new ArrayList<>();
         p1.add(new Person("Хатын", "Ив Зира Бор кызы", 25));
-        p1.add(new Person("Ребьят", "Ив Зира Бор", 19));
+        p1.add(new Person("Ребьят", "Ив Зира Бор", 15));
         p1.add(new Person("Ребьят", "Ив Зира Бо кызы кызы ", 18));
-        p1.add(new Person("Хатын", "Ив Зира Бора кызы кызы кызы", 50));
-        //System.out.println(p1);
+        p1.add(new Person("Хатын", "Ив Зира Бора кызы кызы кызы", 12));
+
+
         Collections.sort(p1, new PersonLength());
         System.out.println(p1);
-
+        List<Person> copy = new ArrayList<>(p1);
+        copy.removeIf(p -> p.getAge() < 18);
+        System.out.println(copy);
     }
 }
